bc03f1f3493f3ef0f688115b49d99fdcafa8ca8c
soroush oracle <soroushoracle@hotmail.com>
2022-03-01 18:46:31 +0330

    use validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
     const { HOST_PORT } = process.env
     const app = await NestFactory.create(AppModule)
+    app.useGlobalPipes(new ValidationPipe({}))
     await app.listen(HOST_PORT)
 }
 bootstrap()
