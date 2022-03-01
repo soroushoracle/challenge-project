ead78e018074e9a45767b3a8a0973e7c071b1469
soroush oracle <soroushoracle@hotmail.com>
2022-03-01 14:16:42 +0330

    configure database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
-@Module({})
+@Module({
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DATABASE_HOST,
+            port: parseInt(process.env.DATABASE_PORT),
+            database: process.env.DATABASE_NAME,
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
+            autoLoadEntities: true,
+            synchronize: true,
+        }),
+    ],
+})
 export class AppModule {}
