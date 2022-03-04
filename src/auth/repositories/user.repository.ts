f798badb020b328e605bbe92ef3f98fd7bf9c1a5
soroush oracle <soroushoracle@hotmail.com>
2022-03-04 16:06:58 +0330

    find by id method on user repository

--- src/auth/repositories/user.repository.ts
+++ src/auth/repositories/user.repository.ts
@@ -1,5 +1,14 @@
+import { NotFoundException } from '@nestjs/common'
 import { EntityRepository, Repository } from 'typeorm'
 import { User } from '../entities/user.entiry'
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+    async findById(id: number): Promise<User> {
+        const user = await this.findOne({ id })
+        if (!user) {
+            throw new NotFoundException('The user not found')
+        }
+        return user
+    }
+}
